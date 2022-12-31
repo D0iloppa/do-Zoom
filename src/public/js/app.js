e69b13abc646c11b866adc68df7bbb795f0334cd
권도일 <doil@d0iloppa.com>
2022-12-31 17:56:10 +0900

    project code direction adjust : delete the testing code and implements some code

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = $("ul");
+const messageForm = $("form");
 // 서버로 연결하는 socket
 const socket = new WebSocket(`ws://${window.location.host}`);
 
@@ -6,52 +8,16 @@ const socket = new WebSocket(`ws://${window.location.host}`);
 socket.addEventListener("open" , ()=>{
     console.log("Wellcome! Connected to Server ✅");
 });
-
-socket.addEventListener("message", (msg)=>{
-    console.log("Just got this : ", msg.data , " from server");
-    
-    const html = `<div style="float:left">서버 : ${msg.data} </div><br>`;
-    $("#chatArea").append(html);
-});
-
 socket.addEventListener("close", ()=>{
     console.log("Bye! Disconnected from Server ❌");
 });
+socket.addEventListener("message", (msg)=>{
+    console.log("New message:", msg.data);
+});
 
-function sendMsgToServer(msg){
-    socket.send(msg);
-}
-
-function sendBtn(){
-    const msg = $("#inputBox").val();
-    if(!msg) return;
-
-    const html = `<div style="float:right">나 : ${msg}</div><br>`;
-    $("#chatArea").append(html);
-    sendMsgToServer(msg);
-    // 클리어
-    $("#inputBox").val('');
-}
-
-
-const badWords = [
-    "시발","미친놈","꺼져","병신"
-];
-let fromMsg = "안녕 시발롬아";
-  
-
-let isBadWordsContains = false;
-
-function bc(){
-    const chk = fromMsg.split(" ");
-    for(let idx in chk){
-        const word = chk[idx];
-        const findIdx =  badWords.findIndex( w => word.indexOf(w) > -1);
-         if( findIdx > -1 ){
-           console.log(findIdx);
-           isBadWordsContains = true;
-           break;
-         };
-       }
-}
- 
+messageForm.on("submit", (event) => {
+    event.preventDefault();
+    const input = $(this).find("input");
+    console.log(input.val());
+   // const input = $(this).input.val();
+});
--- src/server.js
+++ src/server.js
@@ -93,27 +93,12 @@ const handleListen = () =>{
 }
 
 const server = (d_Conf["protocol"] == "http") ? http.createServer(dZoom_server) : https.createServer(dZoom_server);
+
 // webSocket과 server를 모두 같은 포트에 가동시키는 경우 server를 넘겨준다.
 const wss = new WebSocket.Server({ server });
 
-// socket : 연결된 브라우저
-// 해당 나쁜말에 서버는 참지않는다.
-let angryMode = false;
-const badWords = [
-  "시발","미친놈","꺼져","병신"
-];
-const persevereStack = [];
-const randomMsg = [
-  "ㅁㄴㅇ러미ㅏ러마ㅣㄴㅇ러ㅏㅣㅁㄴㅇ러미ㅏㅇㄹ?",
-  "나랏말싸미 듕귁에달아",
-  "피카츄보다 뽀미가 더더더더더 귀여워~",
-  "참새가 봉황의 뜻을 어찌 알리요",
-];
 
-
-
-
-wss.on("connection",(socket)=>{
+wss.on("connection",(socket) => {
   console.log("Connected to Browser ✅");
   socket.send("hello");
 
@@ -122,51 +107,6 @@ wss.on("connection",(socket)=>{
     console.log("Disconnected from the Browser ❌");
   });
 
-  socket.on("message", (msg)=>{
-    const fromMsg = msg.toString();
-    console.log("client : " , fromMsg);
-    //bad word check
-    const chk = fromMsg.split(" ");
-    let isBadWordsContains = false;
-    for(let idx in chk){
-      const word = chk[idx];
-      const findBadWordIdx =  badWords.findIndex( w => word.indexOf(w) > -1);
-         if( findBadWordIdx > -1 ){
-           isBadWordsContains = true;
-           break;
-         };
-    }
-    // 서버는 화나면 무조건 분노만 표출한다.
-    if(angryMode){
-      // 10회 이하로 분노를 표출한다.
-      const count = Math.floor(Math.random() * 10) + 1;
-      for(let i=0; i<count;i++){
-        const randomTime = Math.floor(Math.random() * 100) + 1;
-        setTimeout(()=>{
-          // 분노의 단어
-          const aw = ["적당히 할것이지 뭔 난리야 ㅈ같은색기야" , "개열받네 진짜 븅신같은놈이" , "야이 슈벌럼아 ai가 만만하냐?"];
-          const randomIdx = Math.floor(Math.random() * aw.length);
-          socket.send(aw[randomIdx]);
-          console.log("-" , aw[randomIdx]);
-        }, randomTime * 100);
-      }
-     
-    }else{
-      if(isBadWordsContains){
-        // 인내의 단어
-        const pw = ["뭐라고했냐...?", "적당히 해라??", "하... 못참는다 진짜"];
-        const randomIdx = Math.floor(Math.random() * pw.length);
-        socket.send(pw[randomIdx]);
-        console.log("-" , pw[randomIdx]);
-        persevereStack.push(msg);
-        if(persevereStack.length > 4) angryMode = true;
-      }else{
-        const randomIdx = Math.floor(Math.random() * randomMsg.length);
-        socket.send(`나는 그냥 아무말이나 해. 예를 들면 ${randomMsg[randomIdx]} 과 같은 말..?`);
-        console.log("-" , `나는 그냥 아무말이나 해. 예를 들면 ${randomMsg[randomIdx]} 과 같은 말..?`);
-      }
-    }
-  })
 });
 
 
